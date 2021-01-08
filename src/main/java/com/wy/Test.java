04dbdb483c5ff9259899becb13cf2bc1742a84e2
yunze <yunze.wydl@raycloud.com>
2021-01-08 16:16:06 +0800

    测试

---
+++ src/main/java/com/wy/Test.java
@@ -0,0 +1,16 @@
+package com.wy;
+
+/**
+ * @Classname Test
+ * @Description TODO
+ * @Date 2021/1/8 4:15 下午
+ * @Company 杭州光云科技有限公司
+ * @Author yunze
+ */
+public class Test {
+
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
